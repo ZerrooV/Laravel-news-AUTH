ee86df2e72fe259d826b87740e0b25003a4d691a
ZerrooV <129864192+ZerrooV@users.noreply.github.com>
2024-04-05 14:54:17 +0700

    Update baca.blade.php

--- resources/views/admin/baca.blade.php
+++ resources/views/admin/baca.blade.php
@@ -1,3 +1,22 @@
 @extends('admin.layout.app')
 @section( 'content')
+
+@foreach ($posts as $item)
+<div class="container mt-3 bg-white" style="min-height: 10vh"> <!-- Container baru untuk setiap item -->
+    <tr>
+        <td>
+            <figure>
+                <div>
+                    <blockquote class="blockquote mt-3">
+                        <p>{{$item['title']}}</p>
+                    </blockquote>
+                    <figcaption class="blockquote-footer">
+                        {{$item['content']}}
+                    </figcaption>
+                </div>
+            </figure>
+        </td>
+</div>
+@endforeach
+
 @endsection
